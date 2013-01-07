cd0ca6f9797dafff30209d68b0eae3b224660149
Chuck Little <mdmonk@me.com>
2013-01-07 16:59:10 -0700

    Initial Commit

---
+++ address_to_char.c
@@ -0,0 +1,10 @@
+main(){
+	int i=0;
+	char stuffing[46];
+	
+	for (i=0; i<=40; i+=4)
+	/* *(long *) &stuffing[i] = 0x08 04 83 d5; */
+	/* *(long *) &stuffing[i] = 0x080483d5; */
+/*	*(long *) &stuffing[i] = 0x83d50804;
+	puts(stuffing); */
+}
